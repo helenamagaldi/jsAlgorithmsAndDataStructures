7d89d879c497e1f696a82d501c4e0e7b2dddbf88
Helena Magaldi <helenamagaldi@gmail.com>
2020-08-31 21:30:14 -0300

    1-basicJS: more content

--- 1-basicJS.js
+++ 1-basicJS.js
@@ -1375,3 +1375,59 @@ function abTest(a, b) {=
 //   Do NOT return an array.
 //   Do NOT include quotes (single or double) in the output.
 
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++; //adding 1
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--; // we're not gonna add 1, so --
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Build JavaScript Objects
+// You may have heard the term object before.
+
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+// Here's a sample cat object:
+
+// var cat = {
+//   "name": "Whiskers",
+//   "legs": 4,
+//   "tails": 1,
+//   "enemies": ["Water", "Dogs"]
+// };
+// In this example, all the properties are stored as strings, such as - "name", "legs", and "tails". However, you can also use numbers as properties. You can even omit the quotes for single-word string properties, as follows:
+
+// var anotherObject = {
+//   make: "Ford",
+//   5: "five",
+//   "model": "focus"
+// };
+// However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+// Make an object that represents a dog called myDog which contains the properties "name" (a string), "legs", "tails" and "friends".
+
+// You can set these object properties to whatever values you want, as long as "name" is a string, "legs" and "tails" are numbers, and "friends" is an array.
+
