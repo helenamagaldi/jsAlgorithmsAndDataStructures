ac0b43b6c32f2d640a22cd49616befb29948c617
Helena Magaldi <helenamagaldi@gmail.com>
2021-05-08 18:36:39 -0300

    wtf git

--- 2-ES6.js
+++ 2-ES6.js
@@ -105,4 +105,32 @@ Fix the code so that i declared in the if statement is a separate variable than 
 This exercise is designed to illustrate the difference between how var and let keywords assign scope to the declared variable. When programming a function similar to the one used in this exercise, it is often better to use different variable names to avoid confusion.
 */
 
+function checkScope() {
+  'use strict';
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+
+/*
+ES6: Declare a Read-Only Variable with the const Keyword
+The keyword let is not the only new way to declare variables. In ES6, you can also declare variables using the const keyword.
+
+const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned.
+
+"use strict";
+const FAV_PET = "Cats";
+FAV_PET = "Dogs"; // returns error
+As you can see, trying to reassign a variable declared with const will throw an error. You should always name variables you don't want to reassign using the const keyword. This helps when you accidentally attempt to reassign a variable that is meant to stay constant. A common practice when naming constants is to use all uppercase letters, with words separated by an underscore.
+
+Note: It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). In a later challenge you will see an example of a lowercase variable identifier being used for an array.
+
+Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices, meaning constants should be in all caps.
+*/
+
 
